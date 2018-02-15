a42e3109245b833ddabe89ded5693b91590ac806
Gnadig Peter Levente <gnadig@lab103-08.iit.uni-miskolc.hu>
2018-02-15 12:41:09 +0100

    help meeeee

---
+++ src/my/first/Helloworld.java
@@ -0,0 +1,14 @@
+package my.first;
+
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String message = "hello world";
+		System.out.println(message);
+
+	}
+
+}
