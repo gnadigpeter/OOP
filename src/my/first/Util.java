60a80fd92d3245e730ba286341d27116ba0289ba
gnadig <gnadig@iit323.iit.uni-miskolc.hu>
2018-03-01 12:41:58 +0100

    asda

--- src/my/first/Util.java
+++ src/my/first/Util.java
@@ -2,9 +2,30 @@ package my.first;
 
 public class Util {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void calculate(String operator, double firstnumber, double secondnumber)
 
+	{
+		System.out.print("result: ");
+		switch (operator) {
+		case "-":
+			System.out.println(firstnumber - secondnumber);
+			break;
+		case "+":
+			System.out.println(firstnumber + secondnumber);
+			break;
+		case "/":
+			if (!secondnumber.equals(0.0))
+				System.out.println(firstnumber / secondnumber);
+			else
+				System.out.println("Cannot divide with 0");
+			break;
+		case "*":
+			System.out.println(firstnumber * secondnumber);
+			break;
+		default:
+			System.out.println("unknow operator");
+			break;
+		}
 	}
 
 }
